07ec90ef999638609fc01c0420b8ce2f1f9d974f
YJorange <33817089+YJorange@users.noreply.github.com>
2019-05-25 21:41:24 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+	var lcd1 = ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'];
+	var lcd2 = ['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'];
+	var lcd3 = ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'];
+	var string = ["","",""];
+	for(var i=0;i<str.length;i++){
+		string[0]+=lcd1[parseInt(str[i])]+" ";
+		string[1]+=lcd2[parseInt(str[i])]+" ";
+		string[2]+=lcd3[parseInt(str[i])]+" ";
+	}
+	return string[0].substr(0,string[0].length-1)+'\n'+
+	string[1].substr(0,string[1].length-1)+'\n'+
+	string[2].substr(0,string[2].length-1)+'\n';
 };
